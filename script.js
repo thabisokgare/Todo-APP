6aab54f5b2511a3c7e1b14ec5c6abfb8a2a81996
thabisokgare <thabisoryan8@gmail.com>
2024-11-03 00:38:10 +0200

    UPDATE

--- script.js
+++ script.js
@@ -42,6 +42,26 @@ taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const dataArrIndex = taskData.findIndex((item) =>  item.id === currentTask.id );
-   
+    
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+    };
+
+    if(dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    }
+    taskData.forEach(({id,title,description,date}) => tasksContainer.innerHTML += `
+    <div class = "task" id= "${id}">
+        <p><strong>Title:</strong> ${title}</p>
+        <p><strong>Date:</strong> ${date}</p>
+        <p><strong>Description:</strong> ${description}</p>
+        <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+    </div>
+    `);
+
 
 });
